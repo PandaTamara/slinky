1d13c374f3530da0a5811cb338199921ffac2412
DESKTOP-6PCID01\panda_tamara <lokon38@gmail.com>
2019-05-15 15:08:54 +0300

    moving sass and rules for theme into extra files

--- gulpfile.js
+++ gulpfile.js
@@ -27,19 +27,30 @@ gulp.task('slinky-css', () =>
     .pipe(gulp.dest('dist'))
 )
 
+gulp.task('slinky-themes-css', () =>
+    gulp
+        .src('src/themes/default.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(rename('slinky.default-theme.min.css'))
+        .pipe(gulp.dest('dist/themes'))
+)
+
 gulp.task('docs', () =>
   gulp
-    .src([
-      'dist/slinky.min.css',
-      'dist/slinky.min.js',
-      'dist/slinky.min.js.map'
-    ])
+      .src([
+          'dist/slinky.min.css',
+          'dist/themes/slinky.default-theme.min.css',
+          'dist/slinky.min.js',
+          'dist/slinky.min.js.map'
+      ])
     .pipe(gulp.dest('docs/slinky'))
 )
 
-gulp.task('default', ['slinky-js', 'slinky-css', 'docs'])
+gulp.task('default', ['slinky-js', 'slinky-css', 'slinky-themes-css', 'docs'])
 
 gulp.task('dev', ['slinky-js', 'slinky-css'], () => {
-  gulp.watch('src/slinky.js', ['slinky-js'])
-  gulp.watch('src/slinky.scss', ['slinky-css'])
+    gulp.watch('src/slinky.js', ['slinky-js'])
+    gulp.watch('src/slinky.scss', ['slinky-css'])
+    gulp.watch('src/themes/default.scss', ['slinky-themes-css'])
 })
